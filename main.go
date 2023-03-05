7eca4a11ca35e0811ebd2ddf4d326f15138ee922
nsakki55 <n.sakki55@gmail.com>
2023-03-05 21:03:45 +0900

    add ch17

--- main.go
+++ main.go
@@ -5,14 +5,9 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/nsakki55/go-todo-app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -22,21 +17,7 @@ func main() {
 	}
 }
 
-type Server struct {
-	srv *http.Server
-	l   net.Listener
-}
-
-func NewServer(l net.Listener, mux http.Handler) *Server {
-	return &Server{
-		srv: &http.Server{Handler: mux},
-		l:   l,
-	}
-}
-
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -47,31 +28,7 @@ func run(ctx context.Context) error {
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	s := &http.Server{
-		// 引数で受け取ったnet.Listenerを利用するので、
-		// Addrフィールドは指定しない
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	eg.Go(func() error {
-		// ListenAndServeメソッドではなく、Serveメソッドに変更する
-		if err := s.Serve(l); err != nil &&
-			// http.ErrServerClosed は
-			// http.Server.Shutdown() が正常に終了したことを示すので異常ではない。
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	// グレースフルシャットダウンの終了を待つ。
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
--- server_test.go
+++ server_test.go
@@ -19,7 +19,7 @@ func TestServer_Run(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	eg, ctx := errgroup.WithContext(ctx)
 	mux := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "hello, %s", r.URL.Path[1:])
+		fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
 	})
 	eg.Go(func() error {
 		s := NewServer(l, mux)
@@ -39,7 +39,7 @@ func TestServer_Run(t *testing.T) {
 		t.Fatalf("failed to read body: %v", err)
 	}
 	// HTTPサーバーの戻り値を検証する
-	want := fmt.Sprintf("Hello, %s!", in)
+	want := fmt.Sprintf("Hello, %s", in)
 	if string(got) != want {
 		t.Errorf("want %q, but got %q", want, got)
 	}
